ec6657ba71cc8795d56b8841dd5756807ea58110
amardeep-singh-chauhan <amardeepsingh1105@gmail.com>
2025-02-16 22:55:25 +0530

    Add Header component and integrate it into routing; include logo and navigation links

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Shop from './Pages/Shop/Shop'
+import Header from './Components/Header/Header'
 
 function Routing() {
   return (
-    <Routes>
+    <>
+      <Header />
+      <Routes>
         <Route exact path='/' Component={Home} />
         <Route exact path='/shop' Component={Shop} />
-    </Routes>
+      </Routes>
+    </>
   )
 }
 
