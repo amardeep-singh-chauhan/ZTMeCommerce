f72b8cedb049b8a016adb405554f62d179ba25cb
amardeep-singh-chauhan <amardeepsingh1105@gmail.com>
2025-02-20 00:54:01 +0530

    Add CartItem component and connect Cart to Redux store for cart items display

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import './cart.scss'
 import CustomButton from '../CustomButton/CustomButton'
+import { connect } from 'react-redux'
+import CartItem from '../CartItem/CartItem'
 
-function Cart() {
+function Cart({ cartItems }) {
   return (
     <div className="cart-dropdown">
-        <div className="cart-items" />
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+      <div className="cart-items">
+        {cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)}
+      </div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   )
 }
 
-export default Cart
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+})
+
+export default connect(mapStateToProps)(Cart)
