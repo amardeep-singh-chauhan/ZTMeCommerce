2ae8b071a3c4484cb9db4df0174b99c29995172c
amardeep-singh-chauhan <amardeepsingh1105@gmail.com>
2025-02-20 22:44:02 +0530

    Refactor components to functional components; update Directory, SignIn, and Shop for improved readability and performance

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,47 +1,47 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './signIn.scss'
 import FormInput from '../FormInput/FormInput'
 import CustomButton from '../CustomButton/CustomButton'
 import { signInWithGoogle } from '../../Firebase/firebase.utils'
 
-export class SignIn extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
+function SignIn() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
 
-    handleSubmit = (e) => {
+    const { email, password } = formData;
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        this.setState({ email: '', password: '' })
-    }
+        setFormData({ email: '', password: '' });
+    };
 
-    handleChange = (e) => {
-        const { value, name } = e.target;
-        this.setState({ [name]: value });
-    }
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
 
-    render() {
-        return (
-            <div className="sign-in">
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+    return (
+        <div className="sign-in">
+            <h2>I already have an account</h2>
+            <span>Sign in with your email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput name='email' type='email' handleChange={this.handleChange} value={this.state.email} required label='Email' />
-                    <FormInput name='password' type='password' handleChange={this.handleChange} value={this.state.password} required label='Password' />
-                    <div className="buttons">
-                        <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn >{' '}
-                            Sign in with google{' '}
-                        </CustomButton>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+            <form onSubmit={handleSubmit}>
+                <FormInput name='email' type='email' handleChange={handleChange} value={email} required label='Email' />
+                <FormInput name='password' type='password' handleChange={handleChange} value={password} required label='Password' />
+                <div className="buttons">
+                    <CustomButton type='submit'>Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn >{' '}
+                        Sign in with google{' '}
+                    </CustomButton>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default SignIn
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -3,26 +3,17 @@ import './shop.scss'
 import SHOPDATA from './shopData';
 import PreviewCollection from '../../Components/PreviewCollection.jsx/PreviewCollection';
 
-class Shop extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: SHOPDATA
-        };
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <PreviewCollection key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
+function Shop() {
+    const collections = SHOPDATA;
+    return (
+        <div className="shop-page">
+            {
+                collections.map(({ id, ...otherCollectionProps }) => (
+                    <PreviewCollection key={id} {...otherCollectionProps} />
+                ))
+            }
+        </div>
+    )
 }
 
 export default Shop
