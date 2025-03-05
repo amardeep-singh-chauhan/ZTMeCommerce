91a67f83a3b33b905260a3b3e2c58a1893eae1a5
amardeep-singh-chauhan <amardeepsingh1105@gmail.com>
2025-03-06 00:47:22 +0530

    Add Stripe integration; update dependencies, create StripeUtils, and wrap App component with Elements

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { persistor, store } from './Redux/Store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './Utils/Stripe/StripeUtils';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
-        <App />
+        <Elements stripe={stripePromise} >
+          <App />
+        </Elements>
       </BrowserRouter>
     </PersistGate>
   </Provider>
